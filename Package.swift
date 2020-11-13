225a4b7c5e96300241e408cbbc1d33854eb73f60
Jason Rodriguez <jrodriguez@simplenexus.com>
2020-11-13 09:26:16 -0700

    Support SPM

--- Package.swift
+++ Package.swift
@@ -1,7 +1,11 @@
-// swift-tools-version:4.2
+// swift-tools-version:5.3
 import PackageDescription
 let package = Package(
     name: "LTHPasscodeViewController",
+    defaultLocalization: "en",
+    platforms: [
+        .iOS(.v8)
+    ],
     products: [
         .library(name: "LTHPasscodeViewController", targets: ["LTHPasscodeViewController"])
     ],
